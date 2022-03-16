0e5f4395074839b12b5d3b181886ad5e4eeef77b
Beomjun Kim <nutyworks@gmail.com>
2022-03-16 22:04:08 +0900

    Upload my solutions

---
+++ clippy/target/debug/deps/clippy2-4c89fe523d4883b7.d
@@ -0,0 +1,7 @@
+/Users/nutyworks/Projects/rustlings/exercises/clippy/target/debug/deps/clippy2-4c89fe523d4883b7.rmeta: clippy2.rs
+
+/Users/nutyworks/Projects/rustlings/exercises/clippy/target/debug/deps/clippy2-4c89fe523d4883b7.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
